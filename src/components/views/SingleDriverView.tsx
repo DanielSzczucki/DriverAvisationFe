c4f73c44a7b1422dd855b5679b0d81095b7bafe4
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-04-06 18:28:02 +0200

    Changed naming conventions and organized code in singleload/driver wiev

--- src/components/views/SingleDriverView.tsx
+++ src/components/views/SingleDriverView.tsx
@@ -1,6 +1,6 @@
 import React, { FormEvent, useEffect, useState } from "react";
 import { CreateDriverReq, GetSingleDriverRes, GetSingleLoadRes } from "types";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useAuthHeader } from "react-auth-kit";
 import { config } from "../../utils/config";
 import { SpinnerLoading } from "../common/SpinnerLoading/SpinnerLoading";
@@ -8,6 +8,7 @@ import { fetchData } from "../../utils/fetchData";
 import { ErrorView } from "./ErrorView";
 
 import "./Views.css";
+import { Popup } from "../common/Popup/Popup";
 
 interface GetDriverResStatus {
   message: string;
@@ -17,6 +18,7 @@ interface GetDriverResStatus {
 export const SingleDriverView = () => {
   const { singleDriverId } = useParams();
   const authToken = useAuthHeader();
+  const navigate = useNavigate();
   const [driverInfo, setDriverInfo] = useState<GetSingleDriverRes | null>(null);
   const [loadInfo, setLoadInfo] = useState<GetSingleLoadRes | null>(null);
   const [resultInfo, setResultInfo] = useState<string>();
@@ -32,42 +34,50 @@ export const SingleDriverView = () => {
     loadId: "",
   });
 
+  const [isFormChanged, setIsFormChanged] = useState<boolean>(false);
+  const [isPopupVisible, setIsPopupVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    const getDataForDriver = async () => {
+      try {
+        const driverResUrl = `${config.apiUrl}/driver/${singleDriverId}`;
+        const loadResUrl = `${config.apiUrl}/load/`;
+
+        const driverRes = fetchData(driverResUrl, "GET", authToken());
+        const driverResData: GetDriverResStatus = await driverRes;
+
+        const loadRes = fetchData(
+          `${loadResUrl}${driverResData.driver.loadId}`,
+          "GET",
+          authToken()
+        );
+
+        const loadForDriver: GetSingleLoadRes = await loadRes;
+
+        setDriverInfo(driverResData);
+        setForm({ ...driverResData.driver });
+        setResultInfo(driverResData.message);
+        setLoadInfo(loadForDriver);
+      } catch (e) {
+        setIsPopupVisible(true);
+        <Popup
+          isVisible={true}
+          message={"Cant fetch load data"}
+          onClose={() => setIsPopupVisible(false)}
+        />;
+      }
+    };
+
+    getDataForDriver();
+  }, []);
+
   const updateForm = (key: string, value: any) => {
     setForm((form) => ({
       ...form,
       [key]: value,
     }));
+    setIsFormChanged(true);
   };
-
-  try {
-    const getDataForDriver = async () => {
-      const driverResUrl = `${config.apiUrl}/driver/${singleDriverId}`;
-      const loadResUrl = `${config.apiUrl}/load/`;
-
-      const driverRes = fetchData(driverResUrl, "GET", authToken());
-      const driverResData: GetDriverResStatus = await driverRes;
-
-      const loadRes = fetchData(
-        `${loadResUrl}${driverResData.driver.loadId}`,
-        "GET",
-        authToken()
-      );
-
-      const loadForDriver: GetSingleLoadRes = await loadRes;
-
-      setDriverInfo(driverResData);
-      setForm({ ...driverResData.driver });
-      setResultInfo(driverResData.message);
-      setLoadInfo(loadForDriver);
-    };
-
-    useEffect(() => {
-      getDataForDriver();
-    }, []);
-  } catch (e) {
-    console.log(e);
-    return <ErrorView />;
-  }
 
   const sendForm = async (e: FormEvent) => {
     e.preventDefault();
@@ -84,10 +94,13 @@ export const SingleDriverView = () => {
       console.log(updatedDriver);
 
       setResultInfo(`${updatedDriver.message}`);
-      console.log(resultInfo);
+      setIsPopupVisible(true);
     } catch (e) {
-      console.log(e);
-      return <ErrorView />;
+      setIsPopupVisible(true);
+
+      setTimeout(() => {
+        navigate("/driver");
+      }, 4000);
     }
   };
 
@@ -97,72 +110,80 @@ export const SingleDriverView = () => {
 
   return (
     <>
-      <main className="hide-scrollbar">
-        <form className="box-size glass addForm" onSubmit={sendForm}>
-          <h2>Driver info & edit form</h2>
+      {isPopupVisible ? (
+        <Popup
+          isVisible={isPopupVisible}
+          message={resultInfo}
+          onClose={() => setIsPopupVisible(false)}
+        />
+      ) : (
+        <main className="hide-scrollbar">
+          <form className="box-size glass addForm" onSubmit={sendForm}>
+            <h2>Driver info & edit form</h2>
 
-          <p>
-            Driver:{" "}
-            <span>
-              {driverInfo?.driver.name} {driverInfo?.driver.lastName}
-            </span>
-          </p>
+            <p>
+              Driver:
+              <span>
+                {driverInfo?.driver.name} {driverInfo?.driver.lastName}
+              </span>
+            </p>
 
-          <p>
-            Driver Id: <span>{driverInfo.driver.id}</span>
-          </p>
-          <p>
-            Load Name: <span>{loadInfo?.load.loadName}</span>
-          </p>
-          <p>
-            Counted given loads: <span>{loadInfo?.givenCount}</span>
-          </p>
-          <label>
-            Reference number:
-            <input
-              type="text"
-              value={form.referenceNumber}
-              onChange={(e) => updateForm("referenceNumber", e.target.value)}
-            />
-          </label>
-          <label>
-            Company name:
-            <input
-              type="text"
-              value={form.companyName}
-              onChange={(e) => updateForm("companyName", e.target.value)}
-            />
-          </label>
-          <label>
-            Phone number:
-            <input
-              type="text"
-              value={form.phoneNumber}
-              onChange={(e) => updateForm("phoneNumber", e.target.value)}
-            />
-          </label>
-          <label>
-            Truck number:
-            <input
-              type="text"
-              value={form.truckNumber}
-              onChange={(e) => updateForm("truckNumber", e.target.value)}
-            />
-          </label>
-          <label>
-            Trailer number:
-            <input
-              type="text"
-              value={form.trailerNumber}
-              onChange={(e) => updateForm("trailerNumber", e.target.value)}
-            />
-          </label>
-          <button type="submit">Save</button>
-          <p>
-            <Link to="/driver">Go back to list</Link>
-          </p>
-        </form>
-      </main>
+            <p>
+              Driver Id: <span>{driverInfo.driver.id}</span>
+            </p>
+            <p>
+              Load Name: <span>{loadInfo?.load.loadName}</span>
+            </p>
+            <p>
+              Counted given loads: <span>{loadInfo?.givenCount}</span>
+            </p>
+            <label>
+              Reference number:
+              <input
+                type="text"
+                value={form.referenceNumber}
+                onChange={(e) => updateForm("referenceNumber", e.target.value)}
+              />
+            </label>
+            <label>
+              Company name:
+              <input
+                type="text"
+                value={form.companyName}
+                onChange={(e) => updateForm("companyName", e.target.value)}
+              />
+            </label>
+            <label>
+              Phone number:
+              <input
+                type="text"
+                value={form.phoneNumber}
+                onChange={(e) => updateForm("phoneNumber", e.target.value)}
+              />
+            </label>
+            <label>
+              Truck number:
+              <input
+                type="text"
+                value={form.truckNumber}
+                onChange={(e) => updateForm("truckNumber", e.target.value)}
+              />
+            </label>
+            <label>
+              Trailer number:
+              <input
+                type="text"
+                value={form.trailerNumber}
+                onChange={(e) => updateForm("trailerNumber", e.target.value)}
+              />
+            </label>
+            <button type="submit">Save</button>
+            <p>
+              <Link to="/driver">Go back to list</Link>
+            </p>
+          </form>
+        </main>
+      )}
     </>
   );
 };
--- src/components/views/SingleLoadView.tsx
+++ src/components/views/SingleLoadView.tsx
@@ -1,11 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { GetSingleDriverRes, GetSingleLoadRes } from "types";
+import React, { FormEvent, useEffect, useState } from "react";
+import {
+  CreateLoadReq,
+  GetSingleDriverRes,
+  GetSingleLoadRes,
+  Units,
+} from "types";
 import { config } from "../../utils/config";
 import { useAuthHeader } from "react-auth-kit";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { SpinnerLoading } from "../common/SpinnerLoading/SpinnerLoading";
-
 import { fetchData } from "../../utils/fetchData";
+
+import { Popup } from "../common/Popup/Popup";
+
+interface GetLoadResStatus {
+  message: string;
+  driver: CreateLoadReq;
+}
 
 export const SingleLoadView = () => {
   const { singleLoadId } = useParams();
@@ -17,10 +28,26 @@ export const SingleLoadView = () => {
   const [driverInfo, setDriverInfo] = useState<GetSingleDriverRes | undefined>(
     undefined
   );
+  const [resultInfo, setResultInfo] = useState<string>();
+  const [form, setForm] = useState<CreateLoadReq>({
+    referenceNumber: "",
+    loadName: "",
+    sender: "",
+    recipient: "",
+    forwarder: "",
+    units: Units.other,
+    quantity: 0,
+    weight: 0,
+    count: 0,
+    startDate: "",
+    driverId: "",
+  });
+  const [isFormChanged, setIsFormChanged] = useState<boolean>(false);
+  const [isPopupVisible, setIsPopupVisible] = useState<boolean>(false);
 
   useEffect(() => {
-    try {
-      (async () => {
+    const getDataForLoad = async () => {
+      try {
         const loadResUrl = `${config.apiUrl}/load/${singleLoadId}`;
         const driverResUrl = `${config.apiUrl}/driver/`;
 
@@ -36,12 +63,51 @@ export const SingleLoadView = () => {
 
         setLoadInfo(loadResData);
         setDriverInfo(driverResData);
-      })();
-    } catch (e) {
-      console.log(e);
-      navigate("/load");
-    }
+        setForm({ ...loadResData.load });
+      } catch (e) {
+        setIsPopupVisible(true);
+        <Popup
+          isVisible={true}
+          message={"Cant fetch load data"}
+          onClose={() => setIsPopupVisible(false)}
+        />;
+      }
+    };
+    getDataForLoad();
   }, []);
+
+  const updateForm = (key: string, value: any) => {
+    setForm((form) => ({
+      ...form,
+      [key]: value,
+    }));
+    setIsFormChanged(true);
+  };
+
+  const sendForm = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const loadUpdateRes = fetchData(
+        `${config.apiUrl}/load/${singleLoadId}`,
+        "PUT",
+        authToken(),
+        form
+      );
+
+      const updatedLoad = await loadUpdateRes;
+
+      setResultInfo(`${updatedLoad.message}`);
+      setIsPopupVisible(true);
+    } catch (e) {
+      //popup shows bad message from sendForm > updateLoad.message
+      setIsPopupVisible(true);
+
+      setTimeout(() => {
+        navigate("/load");
+      }, 4000);
+    }
+  };
 
   if (!loadInfo) {
     return <SpinnerLoading />;
@@ -49,30 +115,110 @@ export const SingleLoadView = () => {
 
   return (
     <>
-      <div className="glass views">
-        <h2>{loadInfo.load.loadName}</h2>
-        <p>Load Id: {loadInfo.load.id}</p>
-        <p>Sender: {loadInfo.load.sender}</p>
-        <p>Recipient: {loadInfo?.load.recipient}</p>
-        <p>Frowarder: {loadInfo?.load.forwarder}</p>
-        <p>Units: {loadInfo?.load.units}</p>
-        <p>Quantity: {loadInfo?.load.quantity}</p>
-        <p>Weight: {loadInfo?.load.weight}</p>
-        <p>
-          Driver: {driverInfo?.driver.name ?? "not sign"}
-          {driverInfo?.driver.lastName ?? "not sign"}
-        </p>
-        <p>Truck: {driverInfo?.driver.truckNumber ?? "not sign"}</p>
-        <p>Trailer: {driverInfo?.driver.trailerNumber ?? "not sign"}</p>
-        <p>Counted given loads: {loadInfo?.givenCount ?? "not sign"}</p>
+      {isPopupVisible ? (
+        <Popup
+          isVisible={isPopupVisible}
+          message={resultInfo}
+          onClose={() => setIsPopupVisible(false)}
+        />
+      ) : (
+        <main className="hide-scrollbar">
+          <form className=" box-size glass addForm" onSubmit={sendForm}>
+            <h2>Load info & edit form</h2>
+            <p>Load Id: {loadInfo.load.id}</p>
+            <p>
+              Driver: {driverInfo?.driver.name ?? "not sign"}
+              {driverInfo?.driver.lastName ?? "not sign"}
+            </p>
+            <p>Truck: {driverInfo?.driver.truckNumber ?? "not sign"}</p>
+            <p>Trailer: {driverInfo?.driver.trailerNumber ?? "not sign"}</p>
+            <label>
+              Driver id:
+              <select
+                value={form.driverId}
+                onChange={(e) => updateForm("loadingUnloading", e.target.value)}
+              >
+                <option>{form.driverId}</option>
+              </select>
+            </label>
+            <br />
+            <label>
+              Sender:
+              <input
+                required
+                type="text"
+                value={form.sender}
+                onChange={(e) => updateForm("sender", e.target.value)}
+              />
+            </label>
+            <label>
+              Recipient:
+              <input
+                required
+                type="text"
+                value={form.recipient}
+                onChange={(e) => updateForm("recipient", e.target.value)}
+              />
+            </label>
+            <label>
+              Forwarder:
+              <input
+                required
+                type="text"
+                value={form.forwarder}
+                onChange={(e) => updateForm("forwarder", e.target.value)}
+              />
+            </label>
+            <label>
+              Units:
+              <input
+                required
+                type="text"
+                value={form.units}
+                onChange={(e) => updateForm("units", e.target.value)}
+              />
+            </label>
+            <label>
+              Quantity:
+              <input
+                required
+                type="text"
+                value={form.quantity}
+                onChange={(e) => updateForm("quantity", e.target.value)}
+              />
+            </label>
+            <label>
+              Weight:
+              <input
+                required
+                type="text"
+                value={form.weight}
+                onChange={(e) => updateForm("weight", e.target.value)}
+              />
+            </label>
 
-        <p>
-          <Link to="/load">Go back to list</Link>
-        </p>
-      </div>
+            {isFormChanged ? (
+              <button type="submit">Save</button>
+            ) : (
+              <button
+                //Restore the initial data.
+                onClick={() => {
+                  setForm({ ...loadInfo.load });
+                  setIsFormChanged(false);
+                }}
+              >
+                Cancel
+              </button>
+            )}
+
+            <p>
+              <Link to="/load">Go back to list</Link>
+            </p>
+          </form>
+        </main>
+      )}
     </>
   );
 };
 
-//add new Load option
-// @TODO take and save data to one container and find better hook: useMemo?
+//@TODO - Add a select option with the names of all drivers as a choice to assign to the load
